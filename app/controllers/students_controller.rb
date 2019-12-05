9da92434c2ab09fec9f48f8d2f6dd0a220f308c8
Alyssa Yates <bosslys@icloud.com>
2019-12-05 22:57:46 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,6 +11,10 @@ class StudentsController < ApplicationController
 
   def activate
     @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+
+    redirect '/index'
   end
 
 
